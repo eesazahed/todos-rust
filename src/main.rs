306a99c6e410be188b2c6156fac9d6f25a8d1e6e
Eesa Zahed <eesa.zahed@gmail.com>
2023-03-04 17:52:29 -0500

    format code

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,19 @@ fn main() {
   let mut todos: Vec<String> = vec![String::new(); 0];
 
   loop {
-    println!("\nWhat would you like to do? \n\n (1) Add a todo \n (2) List all todos \n (3) Update a todo \n (4) Delete a todo");
-	
+    println!("\nWhat would you like to do? \n\n (1) Add a todo \n (2) List all todos \n (3) Update a todo \n (4) Delete a todo \n (5) Quit Application \n");
+    
     let input : String = get_input("Type something: ");
-	
+    
     match input.trim() {
       "1" => add_todo(&mut todos),
       "2" => list_todos(&todos),
       "3" => update_todo(&mut todos),
       "4" => delete_todo(&mut todos),
+      "5" => goodbye(),
        _ => println!("\n{}.", "Invalid input".red())
     }
-  }	
+  }    
 }
 
 fn get_input(prompt: &str) -> String {
@@ -37,13 +38,13 @@ fn add_todo(todos: &mut Vec<String>) {
 
 fn list_todos(todos: &Vec<String>) {
   if check_if_empty(todos) {
-  	return;
+    return;
   }
-	
+    
   println!("\n{}\n", "------------------".blue());
 
   for i in 0..todos.len() {
-  	println!(" {index}.) {todo}", index = i + 1, todo = todos[i])
+    println!(" {index}.) {todo}", index = i + 1, todo = todos[i])
   }
 
   println!("\n{}\n", "------------------".blue());
@@ -51,50 +52,50 @@ fn list_todos(todos: &Vec<String>) {
 
 fn update_todo(todos: &mut Vec<String>) {
   if check_if_empty(todos) {
-	return;
+    return;
   }
-	
+    
   let index: String = get_input("Enter the index of the todo you would like to update: ");
   let int_index : i32 = check_index(index, todos);
 
   if int_index == -1 {
-	return
+    return
   }
 
   let array_index : usize = (int_index - 1).try_into().unwrap();
-	
+    
   let ref old_text = &todos[array_index];
   println!("\nOld todo: {old_text}");
-	
+    
   let input : String = get_input("Update todo: ");
-	
+    
   todos[array_index] = input;
   println!("\n{} {}", "Updated todo at index", int_index.to_string().green());
 }
 
 fn delete_todo(todos: &mut Vec<String>) {
   if check_if_empty(todos) {
-	return;
+    return;
   }
 
   let index: String = get_input("Enter the index of the todo you would like to delete: ");
   let int_index : i32 = check_index(index, todos);
 
   if int_index == -1 {
-	return;
+    return;
   }
-	
+    
   let array_index : usize = (int_index - 1).try_into().unwrap();
 
   todos.remove(array_index);
-	
+    
   println!("\n{} {}", "Deleted todo at index", int_index.to_string().green());
 }
 
 fn check_if_empty(todos: &Vec<String>) -> bool {
   if todos.is_empty() {
-	println!("\n{}", "Your todo list is empty.".red());
-	return true;
+    println!("\n{}", "Your todo list is empty.".red());
+    return true;
   }
 
   false
@@ -103,16 +104,21 @@ fn check_if_empty(todos: &Vec<String>) -> bool {
 fn check_index(index: String, todos: &Vec<String>) -> i32 {
   let int_index = match index.parse::<i32>() {
     Ok(i) => i,
- 	Err(_e) => {
-	  println!("\n{}", "Invalid integer.".red());
-	  return -1;
-	},
+    Err(_e) => {
+      println!("\n{}", "Invalid integer.".red());
+      return -1;
+    },
   };
-	
+    
   if int_index < 1 || int_index > (todos.len()).try_into().unwrap() {
-	println!("\n{}", "Invalid integer.".red());
-	return -1;
+    println!("\n{}", "Invalid integer.".red());
+    return -1;
   }
 
   int_index
+}
+
+fn goodbye () {
+  println!("\n{}\n", "Goodbye 👋".yellow());
+  process::exit(0);
 }
